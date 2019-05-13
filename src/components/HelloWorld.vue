deb08e823d3f895c37cd3e33552a5d3e86e1b41d
Ian Campelo <iancampelo@gmail.com>
2019-05-13 18:11:50 -0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Reddit clone /r/Codellit</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-list>
+      <ion-item v-for="post in posts" :key="post.data.id" @click="handleClick(post.data.preview.images[0].source.url)">
+        <img :src="post.data.thumbnail" alt="thumb" class="thumbnail">
+        <ion-label>{{ post.data.title }}</ion-label>
+      </ion-item>
+    </ion-list>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted() {
+    axios.get('https://www.reddit.com/r/Nostalgia.json?raw_json=1')
+      .then(response => {
+        this.posts = response.data.data.children
+      })
+  },
+  data() {
+    return {
+      posts: [],
+      myInput: ''
+    }
+  },
+  methods: {
+    handleClick(imageSrc) {
+      this.$router.push({ name: 'about', params: { imageSrc }})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .thumbnail {
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+    margin-right: 16px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Image</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-img :src="imageSrc"></ion-img>
+    <ion-button @click="$router.go(-1)">Back</ion-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['imageSrc']
+}
+</script>
